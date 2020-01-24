2d52ebd30ef6956fc9371a92994cd3ebd51ceeb6
Evan Blake <evan.peter.blake@gmail.com>
2020-01-24 13:20:20 +1100

    Initial commit

---
+++ generate_raw_image.cpp
@@ -0,0 +1,6 @@
+/* This program is just for testing the main encoder program. It uses OpenCV to decode inputted image files and
+ * outputs raw image files in a user-defined format
+ */
+
+#include <iostream>
+

b094b45f4739773fa442c08c19a9aa2dda8f79f1
Evan Blake <evan.peter.blake@gmail.com>
2024-07-14 14:32:13 +1000

    Bump cmake requirement, create huffman_code.c

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
+# To Do
+
 - Remove Boost and OpenCV as universal dependencies.
 - Fix CMakeLists.txt so that 'generate_raw_jpeg' is only built if Boost and OpenCV are present.
 - Build first iteration of encode_jpeg
---
+++ src/huffman_code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct data_node {
+    char data;
+    unsigned freq;
+} data_node;
+
+data_node getFrequencyTable(char* input_data)
+{
+    // Populate data categories
+    for(int i=0; i < strlen(input_data); i++)
+    {
+
+    } 
+}
+
+
+int main()
+{
+    char* arr = "This is an example of a Huffman tree";
+
+}
+
